2c25d3d1728590847a76d905500d20aa502de6bd
Ian Gunter <iangunterofamerica@yahoo.com>
2020-08-21 08:37:21 -0600

    Add project files.

---
+++ Variables.vb
@@ -0,0 +1,52 @@
+﻿Option Strict On
+Option Explicit On
+Module Variables
+
+    Sub Main()
+        'assigning a variable 
+        Dim myFirstNumber As Integer
+        Dim mySecondNumber As Integer
+        Dim aLittleString As String
+        Dim someSortOfTest As Boolean
+
+
+
+        myFirstNumber = 42
+
+
+        Console.WriteLine(myFirstNumber)
+        Console.ReadLine()
+
+        aLittleString = "some text"
+
+        Console.WriteLine(aLittleString)
+        Console.ReadLine()
+
+        someSortOfTest = True
+
+        Console.WriteLine(someSortOfTest)
+        Console.ReadLine()
+
+        'Break it with wrong data type
+
+        'mySecondNumber = "5"
+
+        'MakeSureToDeclareVaribles
+        'X = 7
+
+        'Console.WriteLine(x)
+        'Console.ReadLine()
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+End Module
